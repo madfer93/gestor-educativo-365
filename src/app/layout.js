5953532531b8e45862f26dd674cb976f209a55ef
madfer93 <madfer1993@gmail.com>
2026-03-02 21:17:55 -0500

    feat: add Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
             <body className={inter.className}>
                 {children}
                 <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
