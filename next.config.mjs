00cfcbc080c82a051ec4c04e591d8ce42c7ade56
madfer93 <madfer1993@gmail.com>
2026-03-04 15:14:44 -0500

    feat: configure Next.js to allow remote images from ibb.co and i.ibb.co.

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ibb.co',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ibb.co',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
